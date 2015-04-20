c1f757470dd156cc7d33647b7f41a27f50d9e21e
Colin Leung <colin@digi3studio.com>
2015-04-20 20:03:44 +0800

    add remove cache.
    Signed-off-by: Colin Leung <colin@digi3studio.com>

--- unittest.php
+++ unittest.php
@@ -130,4 +130,26 @@ class Helper_Unittest {
 	public static function trim_absolute_path(&$str){
 		return preg_replace('/"http:\/\/[^\/]*\//','"ABSOLUTE_PATH/',$str);
 	}
+
+	public static function remove_cache(){
+		self::deleteDir(APPPATH.'cache'.DIRECTORY_SEPARATOR.'ed'.DIRECTORY_SEPARATOR);
+	}
+
+	public static function deleteDir($dirPath) {
+		if (! is_dir($dirPath)) {
+			throw new InvalidArgumentException("$dirPath must be a directory");
+		}
+		if (substr($dirPath, strlen($dirPath) - 1, 1) != '/') {
+			$dirPath .= '/';
+		}
+		$files = glob($dirPath . '*', GLOB_MARK);
+		foreach ($files as $file) {
+			if (is_dir($file)) {
+				self::deleteDir($file);
+			} else {
+				unlink($file);
+			}
+		}
+		rmdir($dirPath);
+	}
 }
